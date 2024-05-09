dbba1126f957046e6926b02850aa926d670af46d
Никита <nikitka-polyakov-2016@mail.ru>
2024-05-09 15:21:32 +0300

    fix ItemsEmpty image size

--- app.constants.ts
+++ app.constants.ts
@@ -1,8 +1,8 @@
-export const APP_WRITE_ID = '6582dee76a0e75b4cda8'
-export const DB_ID = ''
-export const COLLECTION_ITEMS = 'items'
-export const COLLECTION_CATEGORIES = 'categories'
-export const STORAGE_ID = ''  
+// export const APP_WRITE_ID = '6582dee76a0e75b4cda8'
+// export const DB_ID = ''
+// export const COLLECTION_ITEMS = 'items'
+// export const COLLECTION_CATEGORIES = 'categories'
+// export const STORAGE_ID = ''  
 
 export const BRAND_NAME = 'VUE FARM'
 export const SITE_DESCRIPTION = '👨🏼‍🌾 Заказ свежих продуктов прямо с фермы - легко и просто!'
--- components/Home/Cover.vue
+++ components/Home/Cover.vue
@@ -6,7 +6,7 @@
         <div class="bg-slate-300 rounded-3xl h-80 w-full p-10 cover-img text-center flex justify-center items-center">
             <div>
                 <p class="text-white text-3xl font-black">{{ BRAND_NAME }}</p>
-                <p class="text-gray-100 text-xl font-semibold mt-2">Интернет магазин для фермерских продуктов из дома онлайн!</p>
+                <p class="text-gray-100 text-xl font-semibold mt-2">Интернет магазин для заказа фермерских продуктов из дома онлайн!</p>
             </div>
         </div>
     </div>
